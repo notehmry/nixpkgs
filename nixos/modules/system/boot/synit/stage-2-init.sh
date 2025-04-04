4945116aba82ca57cb1b65f59f3e8bb532e44bae
Emery Hemingway <emery@slow.janky.email>
2025-04-04 18:59:20 +0100

    WiP! synit stage2

---
+++ nixos/modules/system/boot/synit/stage-2-init.sh
@@ -0,0 +1,77 @@
+#! @shell@
+
+systemConfig=@systemConfig@
+
+export HOME=/root PATH="@path@"
+
+# Print a greeting.
+echo "\n[1;32m<[1;97m<[1;90m<[1;31m<[1;97m @distroName@ Stage 2 [1;31m>[1;90m>[1;97m>[1;32m>[0m\n"
+
+# Normally, stage 1 mounts the root filesystem read/writable.
+# However, in some environments, stage 2 is executed directly, and the
+# root is read-only.  So make it writable here.
+if [ -z "$container" ]; then
+    mount -n -o remount,rw none /
+fi
+
+if [ ! -c /dev/kmsg ] ; then
+    echo "booting system configuration ${systemConfig}"
+else
+    echo "booting system configuration $systemConfig" > /dev/kmsg
+fi
+
+# Make /nix/store a read-only bind mount to enforce immutability of
+# the Nix store.  Note that we can't use "chown root:nixbld" here
+# because users/groups might not exist yet.
+# Silence chown/chmod to fail gracefully on a readonly filesystem
+# like squashfs.
+chown -f 0:30000 /nix/store
+chmod -f 1775 /nix/store
+if [ -n "@readOnlyNixStore@" ]; then
+    # #375257: Ensure that we pick the "top" (i.e. last) mount so we don't get a false positive for a lower mount.
+    if ! [[ "$(findmnt --direction backward --first-only --noheadings --output OPTIONS /nix/store)" =~ (^|,)ro(,|$) ]]; then
+        if [ -z "$container" ]; then
+            mount --bind /nix/store /nix/store
+        else
+            mount --rbind /nix/store /nix/store
+        fi
+        mount -o remount,ro,bind /nix/store
+    fi
+fi
+
+# Required by the activation script
+install -m 0755 -d /etc
+if [ ! -h "/etc/nixos" ]; then
+    install -m 0755 -d /etc/nixos
+fi
+install -m 01777 -d /tmp
+
+
+# Run the script that performs all configuration activation that does
+# not have to be done at boot time.
+echo "running activation script..."
+$systemConfig/activate
+
+
+# Record the boot configuration.
+ln -sfn "$systemConfig" /run/booted-system
+
+
+# Run any user-specified commands.
+@shell@ @postBootCommands@
+
+mkdir -p \
+  /var/log/synit \
+  /run/etc/syndicate/core \
+  /run/etc/syndicate/services \
+  /run/etc/syndicate/machine \
+
+# This tells Rust programs built with jemallocator to be very aggressive about keeping their
+# heaps small. Synit currently targets small machines. Without this, I have seen the system
+# syndicate-server take around 300MB of heap when doing not particularly much; with this, it
+# takes about 15MB in the same state. There is a performance penalty on being so aggressive
+# about heap size, but it's more important to stay small in this circumstance right now. - tonyg
+export _RJEM_MALLOC_CONF="narenas:1,tcache:false,dirty_decay_ms:0,muzzy_decay_ms:0"
+
+echo "starting Synit..."
+exec @synit-pid1@ "$@"
--- nixos/modules/system/boot/synit/stage-2.nix
+++ nixos/modules/system/boot/synit/stage-2.nix
@@ -5,122 +5,40 @@
   ...
 }:
 
-let
-  inherit (lib)
-    noDepEntry
-    fullDepEntry
-    ;
-
-  stage2Scripts = {
-    shebang = noDepEntry "#! ${pkgs.bash}/bin/bash";
-    defines = fullDepEntry ''
-      systemConfig=@systemConfig@
-      export PATH=${
-        lib.makeBinPath [
-          pkgs.coreutils
-          pkgs.util-linux
-        ]
-      }
-    '' [ "shebang" ];
-
-    activation = fullDepEntry ''
-      # Process the kernel command line.
-      for o in $(</proc/cmdline); do
-          case $o in
-              boot.debugtrace)
-                  # Show each command.
-                  set -x
-                  ;;
-          esac
-      done
-
-      # Print a greeting.
-      echo
-      echo -e "\e[1;32m<<< ${config.system.nixos.distroName} Stage 2 >>>\e[0m"
-      echo
-
-      # Normally, stage 1 mounts the root filesystem read/writable.
-      # However, in some environments, stage 2 is executed directly, and the
-      # root is read-only.  So make it writable here.
-      if [ -z "$container" ]; then
-          mount -n -o remount,rw none /
-      fi
-
-      if [ -c /dev/kmsg ] ; then
-          echo "booting system configuration $systemConfig" > /dev/kmsg
-      else
-          echo "booting system configuration $systemConfig"
-      fi
-
-      # Make /nix/store a read-only bind mount to enforce immutability of
-      # the Nix store.  Note that we can't use "chown root:nixbld" here
-      # because users/groups might not exist yet.
-      # Silence chown/chmod to fail gracefully on a readonly filesystem
-      # like squashfs.
-      chown -f 0:30000 /nix/store
-      chmod -f 1775 /nix/store
-      ${lib.optionalString config.boot.readOnlyNixStore ''
-        # #375257: Ensure that we pick the "top" (i.e. last) mount so we don't get a false positive for a lower mount.
-        if ! [[ "$(findmnt --direction backward --first-only --noheadings --output OPTIONS /nix/store)" =~ (^|,)ro(,|$) ]]; then
-            if [ -z "$container" ]; then
-                mount --bind /nix/store /nix/store
-            else
-                mount --rbind /nix/store /nix/store
-            fi
-            mount -o remount,ro,bind /nix/store
-        fi
-      ''}
-
-      # Required by the activation script
-      install -m 0755 -d /etc
-      if [ ! -h "/etc/nixos" ]; then
-          install -m 0755 -d /etc/nixos
-      fi
-      install -m 01777 -d /tmp
-
-      # Run the script that performs all configuration activation that does
-      # not have to be done at boot time.
-      echo "running activation script..."
-      $systemConfig/activate
-
-      # Record the boot configuration.
-      ln -sfn "$systemConfig" /run/booted-system
-    '' [ "shebang" ];
-
-    postBoot =
-      let
-        cmds = config.boot.postBootCommands;
-      in
-      fullDepEntry (lib.optionalString (cmds != "") "$SHELL ${pkgs.writeText "post-boot.sh" cmds}") [
-        "activation"
-      ];
-
-    powerManagement =
-      let
-        cmds = config.powerManagement.powerUpCommands;
-      in
-      fullDepEntry (lib.optional (cmds != "") "$SHELL ${pkgs.writeText "power-management.sh" cmds}") [
-        "postBoot"
-      ];
-
-    pid1Exec = fullDepEntry "exec ${lib.getExe pkgs.synit-pid1}" [ "powerManagement" ];
-  };
-
-  bootStage2 = pkgs.writeTextFile {
-    name = "stage-2-init.sh";
-    executable = true;
-    text =
-      lib.pipe
-        [ "shebang" "pid1Exec" ]
-        [
-          (lib.textClosureList stage2Scripts)
-          lib.flatten
-          (lib.concatStringsSep "\n")
-        ];
-  };
-in
 {
   config.system.build = lib.mkIf config.synit.enable {
-    inherit bootStage2;
+    bootStage2 = pkgs.replaceVarsWith {
+      src = ./stage-2-init.sh;
+      isExecutable = true;
+      replacements = {
+        shell = "${pkgs.bash}/bin/bash";
+        systemConfig = null; # replaced in ../activation/top-level.nix
+        synit-pid1 = lib.getExe config.synit.pid1.package;
+        inherit (config.boot) readOnlyNixStore;
+        inherit (config.system.nixos) distroName;
+        path = lib.makeBinPath [
+          pkgs.coreutils
+          pkgs.util-linux
+        ];
+        postBootCommands = pkgs.writeText "local-cmds" ''
+          ${config.boot.postBootCommands}
+          ${config.powerManagement.powerUpCommands}
+        '';
+      };
+
+      shell = "${pkgs.bash}/bin/bash";
+      systemConfig = null; # replaced in ../activation/top-level.nix
+      inherit (config.boot) readOnlyNixStore systemdExecutable;
+      inherit (config.system.nixos) distroName;
+      inherit (config.system.build) earlyMountScript;
+      path = lib.makeBinPath [
+        pkgs.coreutils
+        pkgs.util-linux
+      ];
+      postBootCommands = pkgs.writeText "local-cmds" ''
+        ${config.boot.postBootCommands}
+        ${config.powerManagement.powerUpCommands}
+      '';
+    };
   };
 }
