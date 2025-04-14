4c3fbbb3af96fb3ce2cc5e187ac9a73b11078322
Emery Hemingway <emery@slow.janky.email>
2025-04-14 13:35:17 +0100

    synit: add core daemons

--- nixos/modules/system/boot/synit/default.nix
+++ nixos/modules/system/boot/synit/default.nix
@@ -7,13 +7,19 @@
 
 let
   inherit (lib)
+    makeBinPath
+    mapAttrs
     mapAttrs'
     mapAttrsToList
+    mkAfter
     mkMerge
     mkOption
     getExe
     types
     ;
+
+  strOrPath = with types; either str path;
+
   preserves = pkgs.formats.preserves {
     ignoreNulls = true;
     rawStrings = true;
@@ -53,172 +59,223 @@ let
     '';
   };
 
+  daemonSubmodule = types.submodule (
+    { name, ... }:
+    {
+      options = {
+        label = mkOption {
+          type = preserves.literal;
+          default = name;
+          description = ''
+            Label for daemon assertion - `<daemon @label [ … ]>`.
+            Using a list of symbols rather than a concatenated symbol allows patterns
+            to be built that match multiple daemons by prefix. *TODO: is this correct?*
+          '';
+        };
+        argv = mkOption {
+          description = ''
+            Daemon command line.
+            A string is executed in a shell whereas a list of strings is executed directly.
+            See [
+              https://synit.org/book/operation/builtin/daemon.html
+            ](https://synit.org/book/operation/builtin/daemon.html#adding-process-specifications-to-a-service).
+          '';
+          type = with types; either strOrPath (listOf strOrPath);
+        };
+        clearEnv = mkOption {
+          description = ''
+            Whether the Unix process environment is cleared or inherited.
+            If this option is set then {option}`globalEnvironment`
+            is not propagated to this daemon.
+            See [
+              https://synit.org/book/operation/builtin/daemon.html
+            ](https://synit.org/book/operation/builtin/daemon.html#specifying-subprocess-environment-variables).
+          '';
+          type = types.bool;
+          default = false;
+        };
+        dir = mkOption {
+          description = ''
+            Sets the working direcctory of a daemon.
+            See [
+              https://synit.org/book/operation/builtin/daemon.html
+            ](https://synit.org/book/operation/builtin/daemon.html#setting-the-current-working-directory-for-a-subprocess).
+          '';
+          type = with types; nullOr str;
+          default = null;
+        };
+        env = mkOption {
+          description = ''
+            Sets Unix process environment for a daemon.
+            See [
+              https://synit.org/book/operation/builtin/daemon.html
+            ](https://synit.org/book/operation/builtin/daemon.html#specifying-subprocess-environment-variables).
+          '';
+          type = with types; nullOr (attrsOf str);
+          default = null;
+        };
+        path = mkOption {
+          type =
+            with types;
+            listOf (oneOf [
+              str
+              path
+              package
+            ]);
+          default = [ pkgs.coreutils ];
+          defaultText = lib.literalMd "{option}`config.security.wrapperDir` and GNU coreutils";
+          description = ''
+            List of directories to compose into the PATH environmental variable.
+          '';
+        };
+        protocol = mkOption {
+          description = ''
+            Specify a protocol for communicating with a daemon over stdin and stdout.
+            See [
+              https://synit.org/book/operation/builtin/daemon.html
+            ](https://synit.org/book/operation/builtin/daemon.html#speaking-syndicate-network-protocol-via-stdinstdout).
+          '';
+          type = types.enum [
+            "none"
+            "application/syndicate"
+            "text/syndicate"
+          ];
+          default = "none";
+        };
+        readyOnStart = mkOption {
+          description = ''
+            Whether a daemon should be considered ready immediately after startup.
+            See [
+              https://synit.org/book/operation/builtin/daemon.html
+            ](https://synit.org/book/operation/builtin/daemon.html#ready-signalling).
+          '';
+          type = types.bool;
+          default = true;
+        };
+        restart = mkOption {
+          description = ''
+            Daemon restart policy.
+            See [
+              https://synit.org/book/operation/builtin/daemon.html
+            ](https://synit.org/book/operation/builtin/daemon.html#whether-and-when-to-restart).
+          '';
+          type = types.enum [
+            "always"
+            "on-error"
+            "all"
+            "never"
+          ];
+          default = "always";
+        };
+      };
+    }
+  );
+
+  daemonToPreserves = attrs: [
+    attrs.label
+    {
+      argv = builtins.toJSON attrs.argv;
+      env =
+        let
+          env' = lib.optionalAttrs (attrs.env != null) attrs.env;
+        in
+        mapAttrs (k: v: if v == null then false else builtins.toJSON v) (
+          (lib.optionalAttrs (!attrs.clearEnv) config.systemd.globalEnvironment)
+          // env'
+          // {
+            PATH = env'.PATH or "${config.security.wrapperDir}:${makeBinPath attrs.path}";
+          }
+        );
+      inherit (attrs)
+        dir
+        clearEnv
+        readyOnStart
+        restart
+        protocol
+        ;
+    }
+    { _record = "daemon"; }
+  ];
 in
 {
   imports = [
     ./logging.nix
   ];
 
-  options.synit =
-    let
-      strOrPath = with types; either str path;
-    in
-    {
-      enable = lib.mkEnableOption "Synit system layer";
-      syndicate-server.package = lib.mkPackageOption pkgs "syndicate-server" { };
-      pid1.package = lib.mkPackageOption pkgs "synit-pid1" { };
-      pid1.args = lib.mkOption {
-        description = "Command line of the first process spawned by PID1";
-        type = types.listOf strOrPath;
-        default = [
-          logWrapper
-          (lib.getExe cfg.syndicate-server.package)
-          "--inferior"
-          "--config"
-          "${./config}/boot"
-        ];
-        defaultText = lib.literalMD ''The `syndicate-server` wrapped by `s6-log`.'';
-      };
+  options.synit = {
+    enable = lib.mkEnableOption "Synit system layer";
+    syndicate-server.package = lib.mkPackageOption pkgs "syndicate-server" { };
+    pid1.package = lib.mkPackageOption pkgs "synit-pid1" { };
+    pid1.args = lib.mkOption {
+      description = "Command line of the first process spawned by PID1";
+      type = types.listOf strOrPath;
+      default = [
+        logWrapper
+        (lib.getExe cfg.syndicate-server.package)
+        "--inferior"
+        "--config"
+        "${./config}/boot"
+      ];
+      defaultText = lib.literalMD ''The `syndicate-server` wrapped by `s6-log`.'';
+    };
 
-      requires = mkOption {
-        description = ''
-          Services required by the top-level configuration.
-          Each entry is the name of an attr in `config.synit.services`.
-        '';
-        type = with types; listOf str;
-      };
-
-      services = mkOption {
-        description = ''
-          Abstract Syndicate services representing NixOS services.
-        '';
-        type = types.attrsOf (
-          types.submodule (
-            { name, ... }:
-            {
-              options = {
-                label = mkOption {
-                  description = "Label used in assertions to refer to this service.";
-                  type = preserves.literal;
-                  default = [
-                    name
-                    { _record = "service"; }
-                  ];
-                };
-                dependsOn = mkOption {
-                  description = "List of services-states that this service depends on.";
-                  type = types.listOf preserves.literal;
-                };
-              };
-            }
-          )
-        );
-      };
-
+    core = {
       daemons = mkOption {
         description = ''
-          Definitions of daemons to assert into the Synit configuration dataspace.";
+          Definitions of daemons to assert as Synit core services.
+          For each daemon defined in core a `<requires-service <daemon ''${label}>>`
+          assertion is also made.
         '';
-        type = types.attrsOf (
-          types.submodule (
-            { name, ... }:
-            {
-              options = {
-                label = mkOption {
-                  type = preserves.literal;
-                  default = name;
-                  description = ''
-                    Label for daemon assertion - `<daemon @label [ … ]>`.
-                    Using a list of symbols rather than a concatenated symbol allows patterns
-                    to be built that match multiple daemons by prefix. *TODO: is this correct?*
-                  '';
-                };
-                argv = mkOption {
-                  description = ''
-                    Daemon command line.
-                    A string is executed in a shell whereas a list of strings is executed directly.
-                    See [
-                      https://synit.org/book/operation/builtin/daemon.html
-                    ](https://synit.org/book/operation/builtin/daemon.html#adding-process-specifications-to-a-service).
-                  '';
-                  type = with types; either strOrPath (listOf strOrPath);
-                };
-                clearEnv = mkOption {
-                  description = ''
-                    Whether the Unix process environment is cleared or inherited.
-                    See [
-                      https://synit.org/book/operation/builtin/daemon.html
-                    ](https://synit.org/book/operation/builtin/daemon.html#specifying-subprocess-environment-variables).
-                  '';
-                  type = types.bool;
-                  default = false;
-                };
-                dir = mkOption {
-                  description = ''
-                    Sets the working direcctory of a daemon.
-                    See [
-                      https://synit.org/book/operation/builtin/daemon.html
-                    ](https://synit.org/book/operation/builtin/daemon.html#setting-the-current-working-directory-for-a-subprocess).
-                  '';
-                  type = with types; nullOr str;
-                  default = null;
-                };
-                env = mkOption {
-                  description = ''
-                    Sets Unix process environment for a daemon.
-                    See [
-                      https://synit.org/book/operation/builtin/daemon.html
-                    ](https://synit.org/book/operation/builtin/daemon.html#specifying-subprocess-environment-variables).
-                  '';
-                  type = with types; nullOr (attrsOf str);
-                  default = null;
-                };
-                protocol = mkOption {
-                  description = ''
-                    Specify a protocol for communicating with a daemon over stdin and stdout.
-                    See [
-                      https://synit.org/book/operation/builtin/daemon.html
-                    ](https://synit.org/book/operation/builtin/daemon.html#speaking-syndicate-network-protocol-via-stdinstdout).
-                  '';
-                  type = types.enum [
-                    "none"
-                    "application/syndicate"
-                    "text/syndicate"
-                  ];
-                  default = "none";
-                };
-                readyOnStart = mkOption {
-                  description = ''
-                    Whether a daemon should be considered ready immediately after startup.
-                    See [
-                      https://synit.org/book/operation/builtin/daemon.html
-                    ](https://synit.org/book/operation/builtin/daemon.html#ready-signalling).
-                  '';
-                  type = types.bool;
-                  default = true;
-                };
-                restart = mkOption {
-                  description = ''
-                    Daemon restart policy.
-                    See [
-                      https://synit.org/book/operation/builtin/daemon.html
-                    ](https://synit.org/book/operation/builtin/daemon.html#whether-and-when-to-restart).
-                  '';
-                  type = types.enum [
-                    "always"
-                    "on-error"
-                    "all"
-                    "never"
-                  ];
-                  default = "always";
-                };
-              };
-            }
-          )
-        );
+        default = { };
+        type = types.attrsOf daemonSubmodule;
       };
-
     };
+
+    requires = mkOption {
+      description = ''
+        Services required by the top-level configuration.
+        Each entry is the name of an attr in `config.synit.services`.
+      '';
+      type = with types; listOf str;
+    };
+
+    services = mkOption {
+      description = ''
+        Abstract Syndicate services representing NixOS services.
+      '';
+      default = { };
+      type = types.attrsOf (
+        types.submodule (
+          { name, ... }:
+          {
+            options = {
+              label = mkOption {
+                description = "Label used in assertions to refer to this service.";
+                type = preserves.literal;
+                default = [
+                  name
+                  { _record = "service"; }
+                ];
+              };
+              dependsOn = mkOption {
+                description = "List of services-states that this service depends on.";
+                type = types.listOf preserves.literal;
+              };
+            };
+          }
+        )
+      );
+    };
+
+    daemons = mkOption {
+      description = ''
+        Definitions of daemons to assert into the Synit configuration dataspace.";
+      '';
+      default = { };
+      type = types.attrsOf daemonSubmodule;
+    };
+
+  };
 
   config = mkIfSynit {
     assertions = [
@@ -228,27 +285,29 @@ in
       }
     ];
 
+    warnings = [
+      "Synit daemons are using systemd.globalEnvironment until a portable option is introduced."
+    ];
+
     environment.systemPackages = [ cfg.syndicate-server.package ];
 
     environment.etc = mkMerge [
+      (mapAttrs' (name: daemon: {
+        name = "syndicate/core/daemon-${name}.pr";
+        value.source = writePreservesFile "daemon-${name}.pr" [
+          [
+            [
+              daemon.label
+              { _record = "daemon"; }
+            ]
+            { _record = "require-service"; }
+          ]
+          (daemonToPreserves daemon)
+        ];
+      }) cfg.core.daemons)
       {
-        "syndicate/core/.keep".text = "";
         "syndicate/services/daemons.pr".source = writePreservesFile "daemons.pr" (
-          lib.mapAttrsToList (_: attrs: [
-            attrs.label
-            {
-              argv = builtins.toJSON attrs.argv;
-              env = if attrs.env == null then null else lib.mapAttrs builtins.toJSON attrs.env;
-              inherit (attrs)
-                dir
-                clearEnv
-                readyOnStart
-                restart
-                protocol
-                ;
-            }
-            { _record = "daemon"; }
-          ]) config.synit.daemons
+          map daemonToPreserves (builtins.attrValues config.synit.daemons)
         );
       }
       (mapAttrs' (
--- nixos/modules/system/boot/synit/logging.nix
+++ nixos/modules/system/boot/synit/logging.nix
@@ -5,27 +5,10 @@
   ...
 }:
 
-let
-  preserves = pkgs.formats.preserves {
-    ignoreNulls = true;
-    rawStrings = true;
-  };
-in
 {
   config = lib.mkIf config.synit.enable {
-    environment.etc."syndicate/core/syslog.pr".source = preserves.generate "syslog.pr" [
-      [
-        [
-          "s6-socklog"
-          { _record = "daemon"; }
-        ]
-        { _record = "require-service"; }
-      ]
-      [
-        "s6-socklog"
-        { argv = [ (lib.getExe' pkgs.s6 "s6-socklog") ]; }
-        { _record = "daemon"; }
-      ]
-    ];
+    synit.core.daemons.syslog = {
+      argv = [ (lib.getExe' pkgs.s6 "s6-socklog") ];
+    };
   };
 }
