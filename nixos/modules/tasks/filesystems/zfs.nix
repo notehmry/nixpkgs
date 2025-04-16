01b28b53933c5db258b9a70e785f3bc93b160e29
Emery Hemingway <emery@slow.janky.email>
2025-04-16 16:38:54 +0100

    fixup! nixos/zfs: support for non-systemd hosts

--- nixos/modules/tasks/filesystems/zfs.nix
+++ nixos/modules/tasks/filesystems/zfs.nix
@@ -100,7 +100,7 @@ let
       done
       poolReady() {
         pool="$1"
-        state="$("${zpoolCmd}" import ${lib.optionalString config.systemd.enable ''-d "${devNodes}"''} 2>/dev/null | "${awkCmd}" "/pool: $pool/ { found = 1 }; /state:/ { if (found == 1) { print \$2; exit } }; END { if (found == 0) { print \"MISSING\" } }")"
+        state="$("${zpoolCmd}" import -d "${devNodes}" 2>/dev/null | "${awkCmd}" "/pool: $pool/ { found = 1 }; /state:/ { if (found == 1) { print \$2; exit } }; END { if (found == 0) { print \"MISSING\" } }")"
         if [[ "$state" = "ONLINE" ]]; then
           return 0
         else
@@ -354,9 +354,6 @@ in
 
           For guidance on choosing this value, see
           [the ZFS documentation](https://openzfs.github.io/openzfs-docs/Project%20and%20Community/FAQ.html#selecting-dev-names-when-creating-a-pool-linux).
-
-          This option is ignored on hosts not reliant on systemd & udev.
-          Devices will be searched using libblkid instead.
         '';
       };
 
@@ -698,6 +695,13 @@ in
             Hint: for `fileSystems` entries with `fsType = zfs`, the `device` attribute
             should be a zfs dataset name, like `device = "pool/data/set"`.
             This error can be triggered by using an absolute path, such as `"/dev/disk/..."`.
+          '';
+        }
+        {
+          assertion = cfgZfs.devNodes == "/dev/disk/by-id" -> config.systemd.enable;
+          message = ''
+            Cannot search /dev/disk/by-id for ZFS pools because
+            /dev/disk/by-id is an exclusive systemd/udev specific feature.
           '';
         }
       ];
