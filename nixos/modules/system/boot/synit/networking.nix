eee0bea8c8e4609b8cd408c591455d401127ee74
Emery Hemingway <emery@slow.janky.email>
2025-04-25 10:04:26 +0000

    WiP! networking

---
+++ nixos/modules/system/boot/synit/networking.nix
@@ -0,0 +1,72 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.networking;
+  inherit (lib)
+    mapAttrs'
+    optionalAttrs
+    ;
+
+  preserves = pkgs.formats.preserves {
+    ignoreNulls = true;
+    rawStrings = true;
+  };
+  writePreservesFile = preserves.generate;
+
+  quoteStrings =
+    let
+      __toPreserves = lib.generators.toPreserves {
+        ignoreNulls = true;
+        rawStrings = false;
+      };
+    in
+    attrs: attrs // { inherit __toPreserves; };
+
+  assertRecord = label: face: family: attrs: [
+    face
+    family
+    (quoteStrings attrs)
+    { _record = label; }
+  ];
+
+  mkInterfaceFile = face: cfg: {
+    name = "syndicate/network/interface-${face}.pr";
+    value.source = writePreservesFile "interface-${face}.pr" (
+      [
+        [
+          cfg.name
+          (
+            quoteStrings {
+              inherit (cfg)
+                macAddress
+                mtu
+                tempAddress
+                useDHCP
+                ;
+              proxyARP = if cfg.proxyARP then true else null;
+            }
+            // optionalAttrs cfg.wakeOnLan.enable { wakeOnLan.policy = cfg.wakeOnLan.policy; }
+            // optionalAttrs cfg.virtual {
+              owner = cfg.virtualOwner;
+              type = cfg.virtualType;
+            }
+          )
+          { _record = "interface"; }
+        ]
+      ]
+      ++ map (assertRecord "addr" cfg.name "ipv4") cfg.ipv4.addresses
+      ++ map (assertRecord "addr" cfg.name "ipv6") cfg.ipv6.addresses
+      ++ map (assertRecord "route" cfg.name "ipv4") cfg.ipv4.routes
+      ++ map (assertRecord "route" cfg.name "ipv6") cfg.ipv6.routes
+    );
+  };
+in
+{
+  config = lib.mkIf config.synit.enable {
+    environment.etc = mapAttrs' mkInterfaceFile cfg.interfaces;
+  };
+}
---
+++ nixos/modules/system/boot/synit/networking.tcl
@@ -0,0 +1,61 @@
+#!/usr/bin/env -S tclsh
+
+package require syndicate
+
+proc forattr {name attrs body} {
+  preserves::project -unpreserve $attrs ". $name" $name
+  if {$name != ""} $body
+}
+
+syndicate::spawn actor {
+  connect [syndicate::environmentRoute] server {
+
+    during {<addr @ifname #? #_ @address #? @prefixLength #?>} {
+      # Modify interface addresses.
+      set ifaddr "[preserves::unpreserve $address]/$prefixLength"
+      lappend cmdAdd exec ip address add local $ifaddr dev $ifname
+
+      # Add address.
+      catch $cmdAdd err
+      if {$err != ""} { puts stderr "failed to add address $ifaddr to $ifname: $err" }
+
+      # Delete address.
+      set cmdDel [lreplace $cmdAdd 3 3 delete]
+      onStop [list catch $cmdDel]
+
+    } $server
+
+    during {<route @ifname #? #_ @address #? @prefixLength #? @attrs #({ })>} {
+      # Modify routing table.
+      set prefix "[preserves::unpreserve $address]/$prefixLength"
+      lappend cmdAdd exec ip route add to
+
+      forattr type $attrs { lappend cmdAdd $type }
+      lappend cmdAdd $prefix dev $ifname
+
+      forattr via $attrs { lappend cmdAdd via $via }
+
+      foreach options [preserves::project $attrs {. options}] {
+        foreach key [preserves::project -unpreserve $options {.keys}] {
+          preserves::project -unpreserve $options ". $key" val
+          lappend cmdAdd $key $val
+        }
+      }
+
+      # Add route.
+      catch $cmdAdd err
+      if {$err != ""} { puts stderr "failed to add route $prefix to $ifname: $err" }
+
+      # Delete route.
+      set cmdDel [lreplace $cmdAdd 3 3 delete]
+      onStop [list catch $cmdDel]
+
+    } $server
+
+  }
+
+  # Flag that the actor died.
+  onStop {set done 1}
+}
+
+vwait done
