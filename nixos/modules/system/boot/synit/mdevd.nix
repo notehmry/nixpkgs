84c8a6bf76d2692679c90a0271d056f0dbb7363d
Emery Hemingway <emery@slow.janky.email>
2025-05-02 18:43:28 +0100

    synit: configure firmware

--- nixos/modules/system/boot/synit/mdevd.nix
+++ nixos/modules/system/boot/synit/mdevd.nix
@@ -7,6 +7,13 @@
 
 {
   config = lib.mkIf config.synit.enable {
+
+    environment.etc = lib.mkIf config.boot.modprobeConfig.enable {
+      # We don't place this into `extraModprobeConfig` so that stage-1 ramdisk doesn't bloat.
+      "modprobe.d/firmware.conf".text =
+        "options firmware_class path=${config.hardware.firmware}/lib/firmware";
+    };
+
     synit.core.daemons.mdevd = {
       logging.enable = true;
       argv = [
@@ -22,5 +29,17 @@
         pkgs.coreutils
       ];
     };
+
+    system.activationScripts.mdevd = lib.mkIf config.boot.kernel.enable ''
+      # The deprecated hotplug uevent helper is not used anymore
+      if [ -e /proc/sys/kernel/hotplug ]; then
+        echo "" > /proc/sys/kernel/hotplug
+      fi
+
+      # Allow the kernel to find our firmware.
+      if [ -e /sys/module/firmware_class/parameters/path ]; then
+        echo -n "${config.hardware.firmware}/lib/firmware" > /sys/module/firmware_class/parameters/path
+      fi
+    '';
   };
 }
