d2ba920d5ae97ff3b3ea19bb0779e6834a595f47
Emery Hemingway <emery@slow.janky.email>
2025-05-09 11:43:23 +0100

    freerdp: add withSystemd option

--- pkgs/by-name/fr/freerdp/package.nix
+++ pkgs/by-name/fr/freerdp/package.nix
@@ -46,6 +46,7 @@
   SDL2_ttf,
   SDL2_image,
   systemd,
+  withSystemd ? lib.meta.availableOn stdenv.hostPlatform systemd,
   libjpeg_turbo,
   libkrb5,
   libopus,
@@ -145,7 +146,9 @@ stdenv.mkDerivation (finalAttrs: {
     ++ lib.optionals stdenv.hostPlatform.isLinux [
       alsa-lib
       fuse3
-      systemd
+    ]
+    ++ lib.optional withSystemd systemd
+    ++ [
       wayland
       wayland-scanner
     ]
@@ -194,6 +197,7 @@ stdenv.mkDerivation (finalAttrs: {
   );
 
   passthru.tests = {
+    features = { inherit withSystemd withUnfree; };
     inherit remmina;
     inherit gnome-remote-desktop;
   };
